2e9537a59ec0277729883ea65eea35d5448b7458
Rob Wilson <rob.wilson@wealth-dynamix.com>
2023-02-08 15:56:27 +0000

    Loading wait

--- apps/nestjs-api/src/main.ts
+++ apps/nestjs-api/src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import compression from 'compression';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({ origin: 'http://localhost:4200' });
+  app.use(compression());
+
   await app.listen(3000);
 }
 bootstrap();
